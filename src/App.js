2f2b9ebb789ca041358f7472f622403e9a4a96c8
nirbhay244 <kumarnirbhay844@gmail.com>
2023-05-24 07:58:40 +0530

    react learning day-2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+// import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+   
+      
+        <p>
+          Welcome my name is Nirbhay and it is day-2
+        </p>
+       
+  );
+}
+
+export default App;
